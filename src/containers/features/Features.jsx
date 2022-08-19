b5ead22384006d6f5d439da6981ffe535d7defdc
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-08-19 20:29:25 +0400

    v4

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './features.css'
+
+import anywhere from './../../assets/images/icon-access-anywhere.svg'
+import security from './../../assets/images/icon-security.svg'
+import collaboration from './../../assets/images/icon-collaboration.svg'
+import anyfiles from './../../assets/images/icon-any-file.svg'
 
 const Features = () => {
     return (
-        <div>Features</div>
+        <div className='fylo__features-container'>
+            <div className="fylo__features_anywhere">
+                <img src={anywhere} alt="" />
+            </div>
+            <div className="fylo__features_security">
+                <img src={security} alt="" />
+            </div>
+            <div className="fylo__features_collaboration">
+                <img src={collaboration} alt="" />
+            </div>
+            <div className="fylo__features_anyfiles">
+                <img src={anyfiles} alt="" />
+            </div>
+        </div>
     )
 }
 

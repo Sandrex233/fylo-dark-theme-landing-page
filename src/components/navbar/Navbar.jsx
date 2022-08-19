aa6528d1229ee6e263b00d372bce014f2d91962c
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-08-19 16:42:45 +0400

    v4

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
     return (
         <div className='fylo__navbar'>
             <div className='fylo__navbar-links'>
-                <div className='fylo__navbar-links_logo'>
+                <div className='fylo__navbar-logo'>
                     <a href="/"><img src={logo} /></a>
                 </div>
-                <div className='fylo__navbar-links_container'>
-                    <p><a href="#features">Features</a></p>
-                    <p><a href="#team">Team</a></p>
-                    <p><a href="#signin">Sign In</a></p>
-                </div>
+            </div>
+            <div className='fylo__navbar-links_container'>
+                <p><a href="#features">Features</a></p>
+                <p><a href="#team">Team</a></p>
+                <p><a href="#signin">Sign In</a></p>
             </div>
         </div>
     )
--- src/containers/testimonial/Testimonial.jsx
+++ src/containers/testimonial/Testimonial.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Illustration from './../../assets/images/illustration-intro.png'
+import background from './../../assets/images/bg-curvy-desktop.svg'
+import backgroundMobile from './../../assets/images/bg-curvy-mobile.svg'
+import './testimonial.css'
 
 const Testimonial = () => {
     return (
-        <div>Testimonial</div>
+        <div className='fylo__testimonial'>
+            <div className='fylo__testimonial-image'>
+                <div className='background-illustration'>
+                    <img src={Illustration} />
+                </div>
+            </div>
+            <div className='fylo__testimonial-background'>
+                <div className="background-desktop">
+                    <img src={background} />
+                </div>
+                <div className="background-mobile">
+                    <img src={backgroundMobile} />
+                </div>
+            </div>
+            <div className='fylo__testimonial-text'>
+                <h1>All your file in one secure location, <br /> accessible anywhere.</h1>
+                <p>Fylo stores all your most important files in one secure location. <br />
+                    Access them whenever you need, share and collaborate with <br />
+                    friends, family, and co-workers.
+                </p>
+            </div>
+            <div className='fylo__testimonial-button'>
+                <a href="/features"><button>Get started</button></a>
+            </div>
+        </div >
     )
 }
 

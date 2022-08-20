f3ea08598289308c8a2d44a9c262460c51c617a7
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-08-20 19:16:15 +0400

    v5

--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import './signin.css'
+
 const Signin = () => {
     return (
-        <div>Signin</div>
+        <div className='fylo__signin'>
+            <div className="fylo__signin-text">
+                <h1>Get early access today</h1>
+                <p>it only takes a minute to sign up and our free starter tier is extremely generous. If uoi have any <br />
+                    questions, our support team would be happy to help you.
+                </p>
+            </div>
+            <div className="fylo__signin-form">
+                <input type="text" />
+                <button>Get Started For Free</button>
+            </div>
+        </div>
     )
 }
 
